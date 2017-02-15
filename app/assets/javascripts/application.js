2d5bb1db559073ef8492b890efe6ec42fbedb336
diego suarez and loren glenn <diego+loren@>
2017-02-15 12:01:59 -0800

    finished mvp sorta

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,10 +15,12 @@ var block_w = 50;
 var p = 0;
 var setup_x=[];
 var setup_y=[];
+var img = new Image();
+img.src = 'http://i.imgur.com/ZIZ1wdA.jpg';
 
 
 function randomize() {
-  for (var i = 0; i < 50; i++) {
+  for (var i = 0; i < 200; i++) {
     var x = Math.floor((Math.random()* 24)+1)*50;
     var y = Math.floor((Math.random()* 16)+1)*50;
     setup_x.push(x);
@@ -56,29 +58,45 @@ function drawBoard(){
 
     context.strokeStyle = "black";
     context.stroke();
+    context.fillStyle = 'brown'
 }
 
 
 function draw() {
+  context.fillStyle = 'black'
+
   clearCanvas();
   randomize()
-  for (var i = 0; i < 50; i++) {
-    context.fillRect(setup_x[i],setup_y[i],50,50)
+  for (var i = 0; i < 200; i++) {
+    context.drawImage(img,setup_x[i],setup_y[i],50,50)
   }
   drawBoard();
   if (rightKey) block_x += 50;
   else if (leftKey) block_x -= 50;
   if (upKey) block_y -= 50;
   else if (downKey) block_y += 50;
+
   if (block_x <= 0) block_x = 0;
   if ((block_x + block_w) >= WIDTH) block_x = WIDTH - block_w;
   if (block_y <= 0) block_y = 0;
   if ((block_y + block_h) >= HEIGHT) block_y = HEIGHT - block_h;
+  // if(block_x == 100) block_x -=50;
+
+  for (var i = 0; i < 200; i++) {
+    if(block_x == setup_x[i] && block_y == setup_y[i]) {
+      if (rightKey) block_x -= 50;
+      else if (leftKey) block_x += 50;
+      if (upKey) block_y += 50;
+      else if (downKey) block_y -= 50;
+    }
+  }
+
   context.fillRect(block_x,block_y,block_w,block_h);
+
 }
 
 function onKeyDown(evt) {
-  console.log(block_x)
+  console.log(block_x, block_y)
   if (evt.keyCode == 39) rightKey = true;
   else if (evt.keyCode == 37) leftKey = true;
   if (evt.keyCode == 38) upKey = true;
